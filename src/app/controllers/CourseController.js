ec42e5c2d3812614a8aeffafbccc1e23a57b0392
Nguyen Anh Hao <nahao2401@gmail.com>
2025-08-08 22:40:30 +0700

    Update course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,5 +10,37 @@ class CourseController {
       })
       .catch(next);
   }
+
+  // [Get] /courses/:id/edit
+  edit(req, res, next) {
+    Course.findById(req.params.id)
+      .then((course) => {
+        res.render("courses/edit", { course: mongooseToObject(course) });
+      })
+      .catch(next);
+  }
+
+  // [Get] /courses/create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+
+  // [Post] /courses/store
+  store(req, res, next) {
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(formData);
+    course
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((err) => {});
+  }
+
+  // [Put] /courses/:id
+  update(req, res, next) {
+    Course.updateOne({ _id: req.params.id }, req.body)
+      .then(() => res.redirect("/me/stored/courses"))
+      .catch(next);
+  }
 }
 module.exports = new CourseController();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const methodOverride = require("method-override");
 const { engine: handlebars } = require("express-handlebars");
 const app = express();
 const port = 3000;
@@ -14,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extends: true }));
 app.use(express.json());
-
+app.use(methodOverride("_method"));
 //HTTP logger
 app.use(morgan("combined"));
 
@@ -23,6 +24,9 @@ app.engine(
   "hbs",
   handlebars({
     extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b,
+    },
   })
 );
 app.set("view engine", "hbs");
